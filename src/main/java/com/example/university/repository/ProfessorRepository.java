11ef08e597447ac58a2bfec09f90b581c5e59e02
lucky <laxmilaxmi0362@gmail.com>
2024-05-06 10:13:40 +0000

    solution

---
+++ src/main/java/com/example/university/repository/ProfessorRepository.java
@@ -0,0 +1,29 @@
+/*
+ *
+ * You can use the following import statements
+ * 
+ * import java.util.ArrayList;
+ * 
+ */
+
+// Write your code here
+package com.example.university.repository;
+
+import com.example.university.model.Professor;
+import com.example.university.model.Course;
+
+import java.util.ArrayList;
+
+public interface ProfessorRepository {
+    ArrayList<Professor> getProfessors();
+
+    Professor getProfessorById(int professorId);
+
+    Professor addProfessor(Professor professor);
+
+    Professor updateProfessor(int professorId, Professor professor);
+
+    void deleteProfessor(int professorId);
+
+    Course getProfessorCourse(int professorId);
+}
